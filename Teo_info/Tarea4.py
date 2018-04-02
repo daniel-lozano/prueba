436bd2b39b7e75c8ba83835c9024f2f4e8ad88b3
Daniel Lozano Gomez <d.lozano343@colabi2.ad.uniandes.edu.co>
2018-04-02 13:46:25 -0500

    actualizado

--- Teo_info/Tarea4.py
+++ Teo_info/Tarea4.py
@@ -1,21 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sys import argv
+from collections import Counter
 
 name=argv[1]
 
 F=open(name,"r")
 text=F.readlines()
 DATOS=[]
+alfabeto=[]
 
 for i in range(len(text)):
     for j in range(len(text[i])):
             DATOS.append(text[i][j])
-            
     
+print("datos a comprimir")
 print(DATOS)
 
+dictionary=Counter(DATOS)
+print(dictionary.most_common(2))
+print("len dic",len(dictionary))
 
+array=dictionary.elements()
+print(type(array))
+
+"""
+for i in range(1,len(array)):
+    if(array[i]!=array[i-1]):
+        alfabeto.append(array[i])
+print(alfabeto)
+"""
     
 
     
