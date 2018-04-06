ce996b24d9569441b3f95c552834eafb80398d41
daniel-lozano <d.lozano343@uniandes.edu.co>
2018-04-05 22:53:33 -0500

    hoffman coding

--- Teo_info/TAREA4/HOFFMAN/Codifica_hoffman.py
+++ Teo_info/TAREA4/HOFFMAN/Codifica_hoffman.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sys import argv
 from collections import Counter
+from copy import deepcopy
 
 name=argv[1]
 F=open(name,"r")
@@ -21,40 +22,96 @@ binario=[]
 dictionary=Counter(DATOS)
 array=dictionary.most_common()
 
-def copying_but_last(A):
-    B=[]
-    for i in range(len(A)-1):
-        B.append(A[i])
-    return B
-
-def searching(A,a):
-    
-    for i in range(len(A)):
-        if(A[i]==a):
-            return i
-    return -1
-
-
-
-
 letras=[]
 valores=[]
+codigo=[]
+
 for i in range(len(array)):
     letras.append(array[i][0])
     valores.append(array[i][1])
+    codigo.append("")
+
+#-------------------------Useful functions------------------------------------------------------
+def searching(A,a):
+    for i in range(len(A)):
+        if(A[i]==a):
+            return True
+    return False
+
+def min_2(A):
+    min1=min(A)
+    index1=np.argmin(A)
+    A[index1]=np.inf
+    min2=min(A)
+    index2=np.argmin(A)
+    return [index1,index2]
+
+def reorganiza(A):
+    C=[]
+    marker=False
+    for i in range(len(A)-1):
+        if(A[i][1]>=A[-1][1] or marker==True):
+            C.append(A[i])
+        
+        if(A[i][1]<A[-1][1] and marker==False):
+            C.append(A[-1])
+            C.append(A[i])
+            marker=True
+    return C
+
+def repite(A):
     
-print(letras,valores)
+    for i in A:
+        cont=0
+        for j in A:
+            if(i==j):
+                cont+=1
+        if(cont>=2):
+            return True
+    return False
+#---------------------------preparing the new alphabet-------------------------------------------
+old=[]
+for i in range(len(array)):
+    old.append([array[i][0],array[i][1]])
+
+#---------------------------Writing the Binary code--------------------------------------------
+
+for i in range(len(array)-1):
+    
+    for j in range(len(letras)):
+        
+        if(searching(old[-2][0],letras[j])==True):
+            codigo[j]+="1"
+        if(searching(old[-1][0],letras[j])==True):
+            codigo[j]+="0"
+    
+    
+    act=old[:-1]
+    act[-1][0]+=old[-1][0]
+    act[-1][1]+=old[-1][1]
+    
+    old=reorganiza(act)
+    new=deepcopy(old)
+print(letras)
+print(codigo)
+print(repite(codigo))
+
+#-----------------------Writing code alphabet-------------------------------------------------
+
+CODIGO=open("codigo_"+name,"w")
+for i in range(len(letras)):
+    CODIGO.write(letras[i]+" "+codigo[i]+"\n")
+CODIGO.close()
+
+COMP=open("comp_"+name,"w")
+
+for i in range(len(DATOS)):
+    for j in range(len(letras)):
+        if(DATOS[i]==letras[j]):
+            COMP.write(codigo[j])
+
+COMP.close()
 
 
-
-
-
-
-
-"""
-array2=copying_but_last(array)
-print(array)
-print(array2)
-"""
 
 
